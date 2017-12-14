e2479bac6a97e21237ca3e2b875dee43d56d2b9a
Ritesh Dalvi <riteshdalvi270@gmail.com>
2017-12-14 00:42:23 -0600

    Successfully implemented all the functions.

--- src/main/java/com/boot/controller/ShipWreskResource.java
+++ src/main/java/com/boot/controller/ShipWreskResource.java
@@ -1,15 +1,41 @@
 package com.boot.controller;
+import java.util.List;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.boot.model.Shipwreck;
+import com.boot.resource.ShipwreckStub;
+
 @RestController
-@RequestMapping("/v1/api/")
+@RequestMapping("/api/v1")
 public class ShipWreskResource {
 
-	@RequestMapping(value="shipwreck",method=RequestMethod.GET)
-	public void getAllShips() {
-		
+	@RequestMapping(value="shipwrecks",method=RequestMethod.GET)
+	public List<Shipwreck> getAllShips() {
+		return ShipwreckStub.list();
+	}
+	
+	@RequestMapping(value="shipwrecks", method=RequestMethod.POST)
+	public Shipwreck addShipWreck(@RequestBody Shipwreck Shipwreck) {
+		return ShipwreckStub.create(Shipwreck);
+	}
+	
+	@RequestMapping(value="shipwrecks/{id}",method=RequestMethod.GET)
+	public Shipwreck getShipWreck(@PathVariable Long id) {
+		return ShipwreckStub.get(id);
+	}
+	
+	@RequestMapping(value="shipwrecks/{id}", method=RequestMethod.DELETE)
+	public Shipwreck deleteShipWreck(@PathVariable Long id) {
+		return ShipwreckStub.delete(id);
+	}
+	
+	@RequestMapping(value="shipwrecks/{id}",method=RequestMethod.PUT)
+	public Shipwreck putShipWreck(@PathVariable Long id, @RequestBody Shipwreck shipwreck) {
+		return ShipwreckStub.update(id, shipwreck);
 	}
 }
